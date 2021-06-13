a67bf2a02f8bc7aa3fa5935f8779df36113d487c
basavarajamogi <basavarajamogi@gmail.com>
2021-06-12 22:21:59 -0700

    second commit

---
+++ azure_irrigation/config.py
@@ -0,0 +1,5 @@
+ACCOUNT_NAME = ""
+ACCOUNT_KEY = ""
+CONNECTION_STRING = ""
+CONTAINER = "dockerazure_irri"
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg','tiff'])
--- azure_irrigation/irriapp.py
+++ azure_irrigation/irriapp.py
@@ -3,6 +3,20 @@ from werkzeug.utils import secure_filename
 from azure.storage.blob import BlobServiceClient
 import os
 app = Flask(__name__,static_folder='static', static_url_path='')
+#Details of azure account
+app.config.from_pyfile('config.py')
+account = app.config['ACCOUNT_NAME']   # Azure account name
+key = app.config['ACCOUNT_KEY']      # Azure Storage account access key  
+connect_str = app.config['CONNECTION_STRING']
+container = app.config['CONTAINER'] # Container name
+allowed_ext = app.config['ALLOWED_EXTENSIONS'] # List of accepted extensions
+
+blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in allowed_ext
+
 @app.route('/')
 def home():
     return render_template('index.html')
